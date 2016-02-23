c7e6553c7fcb568b454d7a1b1bf9e845a76a86dd
Shubhendu Tripathi <shtripat@redhat.com>
2016-02-23 17:14:17 +0530

    bigfin: Block device management apis
    Change-Id: Ic185adc54a6a4e5101c136a25fcac20fd53c23de
    Signed-off-by: Shubhendu Tripathi <shtripat@redhat.com>

--- provider/block_devices.go
+++ provider/block_devices.go
@@ -211,3 +211,197 @@ func createBlockStorage(ctxt string, clusterId uuid.UUID, backingStorage string,
 	return true
 }
 
+func (s *CephProvider) DeleteBlockDevice(req models.RpcRequest, resp *models.RpcResponse) error {
+	ctxt := req.RpcRequestContext
+
+	cluster_id_str := req.RpcRequestVars["cluster-id"]
+	cluster_id, err := uuid.Parse(cluster_id_str)
+	if err != nil {
+		logger.Get().Error("%s - Error parsing the cluster id: %s. error: %v", ctxt, cluster_id_str, err)
+		*resp = utils.WriteResponse(http.StatusBadRequest, fmt.Sprintf("Error parsing the cluster id: %s", cluster_id_str))
+		return err
+	}
+
+	storage_id_str := req.RpcRequestVars["storage-id"]
+	storage_id, err := uuid.Parse(storage_id_str)
+	if err != nil {
+		logger.Get().Error("%s - Error parsing the storage id: %s. error: %v", ctxt, storage_id_str, err)
+		*resp = utils.WriteResponse(http.StatusBadRequest, fmt.Sprintf("Error parsing the storage id: %s", storage_id_str))
+		return err
+	}
+	blockdevice_id_str := req.RpcRequestVars["blockdevice-id"]
+	blockdevice_id, err := uuid.Parse(blockdevice_id_str)
+	if err != nil {
+		logger.Get().Error("%s - Error parsing the block device id: %s. error: %v", ctxt, blockdevice_id_str, err)
+		*resp = utils.WriteResponse(http.StatusBadRequest, fmt.Sprintf("Error parsing the block device id: %s", blockdevice_id_str))
+		return err
+	}
+
+	asyncTask := func(t *task.Task) {
+		for {
+			select {
+			case <-t.StopCh:
+				return
+			default:
+				sessionCopy := db.GetDatastore().Copy()
+				defer sessionCopy.Close()
+				var cluster models.Cluster
+				var storage models.Storage
+				var blockDevice models.BlockDevice
+
+				t.UpdateStatus("Started ceph provider block device deletion: %v", t.ID)
+				t.UpdateStatus("Getting details of cluster")
+				coll := sessionCopy.DB(conf.SystemConfig.DBConfig.Database).C(models.COLL_NAME_STORAGE_CLUSTERS)
+				if err := coll.Find(bson.M{"clusterid": *cluster_id}).One(&cluster); err != nil {
+					utils.FailTask("Error getting details of cluster", fmt.Errorf("%s - %v", ctxt, err), t)
+					return
+				}
+				t.UpdateStatus("Getting details of storage entity")
+				coll1 := sessionCopy.DB(conf.SystemConfig.DBConfig.Database).C(models.COLL_NAME_STORAGE)
+				if err := coll1.Find(bson.M{"storageid": *storage_id}).One(&storage); err != nil {
+					utils.FailTask("Error getting details of storage entity", fmt.Errorf("%s - %v", ctxt, err), t)
+					return
+				}
+				t.UpdateStatus("Getting details of block device entity")
+				coll2 := sessionCopy.DB(conf.SystemConfig.DBConfig.Database).C(models.COLL_NAME_BLOCK_DEVICES)
+				if err := coll2.Find(bson.M{"clusterid": *cluster_id, "storageid": *storage_id, "id": *blockdevice_id}).One(&blockDevice); err != nil {
+					utils.FailTask("Error getting details of block device entity", fmt.Errorf("%s - %v", ctxt, err), t)
+					return
+				}
+				t.UpdateStatus("Getting a mon from cluster")
+				monnode, err := GetRandomMon(*cluster_id)
+				if err != nil {
+					utils.FailTask(fmt.Sprintf("Error getting mon node details for cluster: %v", *cluster_id), fmt.Errorf("%s - %v", ctxt, err), t)
+					return
+				}
+				t.UpdateStatus("Deleting block device")
+				cmd := fmt.Sprintf("rbd rm %s --cluster %s --pool %s", blockDevice.Name, cluster.Name, storage.Name)
+				ok, err := cephapi_backend.ExecCmd(monnode.Hostname, *cluster_id, cmd)
+				if err != nil || !ok {
+					utils.FailTask(fmt.Sprintf("Deletion of block device failed on cluster: %s", *cluster_id), fmt.Errorf("%s - %v", ctxt, err), t)
+					return
+				} else {
+					t.UpdateStatus("Removing the block device entity from DB")
+					if err := coll2.Remove(bson.M{"clusterid": *cluster_id, "storageid": *storage_id, "id": *blockdevice_id}); err != nil {
+						utils.FailTask(fmt.Sprintf("Error removing block device entity from DB for cluster: %d", *cluster_id), fmt.Errorf("%s - %v", ctxt, err), t)
+						return
+					}
+				}
+				t.UpdateStatus("Success")
+				t.Done(models.TASK_STATUS_SUCCESS)
+				return
+			}
+		}
+	}
+	if taskId, err := bigfin_task.GetTaskManager().Run("CEPH-DeleteBlockDevice", asyncTask, 120*time.Second, nil, nil, nil); err != nil {
+		logger.Get().Error("%s - Task creation failed for delete block device %v on cluster: %v. error: %v", ctxt, *blockdevice_id, *cluster_id, err)
+		*resp = utils.WriteResponse(http.StatusInternalServerError, "Task creation failed for block device deletion")
+		return err
+	} else {
+		*resp = utils.WriteAsyncResponse(taskId, fmt.Sprintf("Task Created for delet block device %v on cluster: %v", *blockdevice_id, *cluster_id), []byte{})
+	}
+	return nil
+}
+
+func (s *CephProvider) ResizeBlockDevice(req models.RpcRequest, resp *models.RpcResponse) error {
+	ctxt := req.RpcRequestContext
+
+	cluster_id_str := req.RpcRequestVars["cluster-id"]
+	cluster_id, err := uuid.Parse(cluster_id_str)
+	if err != nil {
+		logger.Get().Error("%s - Error parsing the cluster id: %s. error: %v", ctxt, cluster_id_str, err)
+		*resp = utils.WriteResponse(http.StatusBadRequest, fmt.Sprintf("Error parsing the cluster id: %s", cluster_id_str))
+		return err
+	}
+
+	storage_id_str := req.RpcRequestVars["storage-id"]
+	storage_id, err := uuid.Parse(storage_id_str)
+	if err != nil {
+		logger.Get().Error("%s - Error parsing the storage id: %s. error: %v", ctxt, storage_id_str, err)
+		*resp = utils.WriteResponse(http.StatusBadRequest, fmt.Sprintf("Error parsing the storage id: %s", storage_id_str))
+		return err
+	}
+	blockdevice_id_str := req.RpcRequestVars["blockdevice-id"]
+	blockdevice_id, err := uuid.Parse(blockdevice_id_str)
+	if err != nil {
+		logger.Get().Error("%s - Error parsing the block device id: %s. error: %v", ctxt, blockdevice_id_str, err)
+		*resp = utils.WriteResponse(http.StatusBadRequest, fmt.Sprintf("Error parsing the block device id: %s", blockdevice_id_str))
+		return err
+	}
+	var request struct {
+		Size string `json:"size"`
+	}
+	if err := json.Unmarshal(req.RpcRequestData, &request); err != nil {
+		logger.Get().Error("%s - Unbale to parse the request. error: %v", ctxt, err)
+		*resp = utils.WriteResponse(http.StatusBadRequest, fmt.Sprintf("Unbale to parse the request. error: %v", err))
+		return err
+	}
+	sizeMBs := utils.SizeFromStr(request.Size) / 1024
+
+	asyncTask := func(t *task.Task) {
+		for {
+			select {
+			case <-t.StopCh:
+				return
+			default:
+				sessionCopy := db.GetDatastore().Copy()
+				defer sessionCopy.Close()
+				var blockDevice models.BlockDevice
+				var cluster models.Cluster
+				var storage models.Storage
+
+				t.UpdateStatus("Started ceph provider block device deletion: %v", t.ID)
+				t.UpdateStatus("Getting details of cluster")
+				coll := sessionCopy.DB(conf.SystemConfig.DBConfig.Database).C(models.COLL_NAME_STORAGE_CLUSTERS)
+				if err := coll.Find(bson.M{"clusterid": *cluster_id}).One(&cluster); err != nil {
+					utils.FailTask("Error getting details of cluster", fmt.Errorf("%s - %v", ctxt, err), t)
+					return
+				}
+				t.UpdateStatus("Getting details of storage")
+				coll1 := sessionCopy.DB(conf.SystemConfig.DBConfig.Database).C(models.COLL_NAME_STORAGE)
+				if err := coll1.Find(bson.M{"storageid": *storage_id}).One(&storage); err != nil {
+					utils.FailTask("Error getting details of storage", fmt.Errorf("%s - %v", ctxt, err), t)
+					return
+				}
+				t.UpdateStatus("Getting details of block device entity")
+				coll2 := sessionCopy.DB(conf.SystemConfig.DBConfig.Database).C(models.COLL_NAME_BLOCK_DEVICES)
+				if err := coll2.Find(bson.M{"clusterid": *cluster_id, "storageid": *storage_id, "id": *blockdevice_id}).One(&blockDevice); err != nil {
+					utils.FailTask("Error getting details of block device entity", fmt.Errorf("%s - %v", ctxt, err), t)
+					return
+				}
+				t.UpdateStatus("Getting a mon from cluster")
+				monnode, err := GetRandomMon(*cluster_id)
+				if err != nil {
+					utils.FailTask(fmt.Sprintf("Error getting mon node details for cluster: %v", *cluster_id), fmt.Errorf("%s - %v", ctxt, err), t)
+					return
+				}
+				t.UpdateStatus("Resizing block device")
+				cmd := fmt.Sprintf("rbd resize --image %s --cluster %s --size %d --pool %s", blockDevice.Name, cluster.Name, sizeMBs, storage.Name)
+				ok, err := cephapi_backend.ExecCmd(monnode.Hostname, *cluster_id, cmd)
+				if err != nil || !ok {
+					utils.FailTask(fmt.Sprintf("Resizing of block device failed on cluster: %s", *cluster_id), fmt.Errorf("%s - %v", ctxt, err), t)
+					return
+				} else {
+					t.UpdateStatus("Updating the block device entity in DB")
+					if err := coll2.Update(
+						bson.M{"clusterid": *cluster_id, "storageid": *storage_id, "id": *blockdevice_id},
+						bson.M{"$set": bson.M{"size": request.Size}}); err != nil {
+						utils.FailTask(fmt.Sprintf("Error updating block device entity in DB for cluster: %d", *cluster_id), fmt.Errorf("%s - %v", ctxt, err), t)
+						return
+					}
+				}
+				t.UpdateStatus("Success")
+				t.Done(models.TASK_STATUS_SUCCESS)
+				return
+			}
+		}
+	}
+	if taskId, err := bigfin_task.GetTaskManager().Run("CEPH-ResizeBlockDevice", asyncTask, 120*time.Second, nil, nil, nil); err != nil {
+		logger.Get().Error("%s - Task creation failed for resize block device %v on cluster: %v. error: %v", ctxt, *blockdevice_id, *cluster_id, err)
+		*resp = utils.WriteResponse(http.StatusInternalServerError, "Task creation failed for block device resize")
+		return err
+	} else {
+		*resp = utils.WriteAsyncResponse(taskId, fmt.Sprintf("Task Created for resize block device %v on cluster: %v", *blockdevice_id, *cluster_id), []byte{})
+	}
+	return nil
+}
