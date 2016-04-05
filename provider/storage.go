e2c5622b78b46a6d47a6ca85b3b0808c6a131fe8
nthomas-redhat <nishusemail@gmail.com>
2016-04-05 15:48:23 +0530

    bigfin: create cluster integration
    Integrated the cluster creation flow with
    ceph-installer
    
    Change-Id: Ida6c5eac84d3e451d77184124389d9962125e2fe
    Signed-off-by: nthomas-redhat <nishusemail@gmail.com>

--- provider/storage.go
+++ provider/storage.go
@@ -230,7 +230,7 @@ func createPool(ctxt string, clusterId uuid.UUID, request models.AddStorageReque
 			pgNum = DerivePgNum(clusterId, request.Size, request.Replicas)
 		}
 	}
-	rulesetmapval, ok := cluster.Options["rulesetmap"]
+	/*rulesetmapval, ok := cluster.Options["rulesetmap"]
 	if !ok {
 
 		logger.Get().Error("Error getting the ruleset for cluster: %s", cluster.Name)
@@ -243,9 +243,9 @@ func createPool(ctxt string, clusterId uuid.UUID, request models.AddStorageReque
 		logger.Get().Error("Error getting the ruleset for cluster: %s", cluster.Name)
 		return nil, false
 	}
-	ruleset := rulesetval.(map[string]interface{})
+	ruleset := rulesetval.(map[string]interface{})*/
 
-	ok = true
+	ok := true
 	if request.Type == models.STORAGE_TYPE_ERASURE_CODED {
 		// cmd := fmt.Sprintf("ceph --cluster %s osd pool create %s %d %d erasure %s", cluster.Name, request.Name, uint(pgNum), uint(pgNum), request.Options["ecprofile"])
 		// ok, _, err = cephapi_backend.ExecCmd(monnode.Hostname, clusterId, cmd, ctxt)
@@ -259,7 +259,8 @@ func createPool(ctxt string, clusterId uuid.UUID, request models.AddStorageReque
 			quotaMaxObjects,
 			quotaMaxBytes,
 			request.Options["ecprofile"],
-			ruleset["rulesetid"].(int),
+			//ruleset["rulesetid"].(int),
+			0,
 			ctxt)
 	} else {
 		ok, err = cephapi_backend.CreatePool(
@@ -270,7 +271,8 @@ func createPool(ctxt string, clusterId uuid.UUID, request models.AddStorageReque
 			request.Replicas,
 			quotaMaxObjects,
 			quotaMaxBytes,
-			ruleset["rulesetid"].(int),
+			//ruleset["rulesetid"].(int),
+			0,
 			ctxt)
 	}
 	if err != nil || !ok {
