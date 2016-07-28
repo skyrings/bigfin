625f2043d3528a01bb68ae54060c547d2475b967
Shubhendu <shtripat@redhat.com>
2016-07-28 10:16:15 +0530

    bigfin: Consider next 2's power if pgnum < osd count
    If calculated pgnum value is less than no of OSDs
    consider next 2's power as pgnum.
    
    Change-Id: Ie5cd92f237013e2f2bcae615c4173da3945e2fdf
    Signed-off-by: Shubhendu <shtripat@redhat.com>

--- provider/block_devices.go
+++ provider/block_devices.go
@@ -348,7 +348,7 @@ func (s *CephProvider) ResizeBlockDevice(req models.RpcRequest, resp *models.Rpc
 					return
 				}
 				t.UpdateStatus("Resizing block device")
-				cmd := fmt.Sprintf("rbd resize --image %s --cluster %s --size %d --pool %s", blockDevice.Name, cluster.Name, sizeMBs, storage.Name)
+				cmd := fmt.Sprintf("rbd resize --image %s --cluster %s --size %f --pool %s", blockDevice.Name, cluster.Name, sizeMBs, storage.Name)
 				ok, _, err := cephapi_backend.ExecCmd(monnode.Hostname, *cluster_id, cmd, ctxt)
 				if err != nil || !ok {
 					utils.FailTask(fmt.Sprintf("Resizing of block device failed on cluster: %s", *cluster_id), fmt.Errorf("%s - %v", ctxt, err), t)
